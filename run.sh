47b7ad8a829b925114c67ee35de0f13f2a5d0ee4
Juni May <juni_may@outlook.com>
2022-11-21 18:08:31 +0800

    refactor: use validation set every train epoch

--- run.sh
+++ run.sh
@@ -1,10 +1,29 @@
 python main.py \
     --num-classes 5 --num-workers 12 \
     --num-epochs 80 --batch-size 32  \
-    --valid-data-dir 'data/DDR/test' \
+    --valid-data-dir 'data/DDR/valid' \
     --log-dir 'tf-logs' --lr 1e-3 --depth 4 \
     --input-size 480 --kernel-size 1 --backbone-lr 2e-5 \
     --backbone convnext_base --pretrained --timestamp 'convnext-model-pretrained-unfreezed'
+
+
+python main.py \
+    --num-classes 5 --num-workers 12 \
+    --num-epochs 80 --batch-size 32  \
+    --valid-data-dir 'data/DDR/test' \
+    --log-dir 'tf-logs' --lr 1e-3 --depth 4 \
+    --input-size 480 --kernel-size 1 --backbone-lr 2e-5 \
+    --backbone convnext_base --pretrained --timestamp 'convnext-model-pretrained-unfreezed' --valid
+
+python main.py                       \
+    --num-classes 5 --num-workers 12 \
+    --num-epochs 80 --batch-size 128 \
+    --valid-data-dir 'data/DDR/valid' \
+    --log-dir 'tf-logs' --lr 1e-3    \
+    --input-size 480 --kernel-size 9 \
+    --dim 384 --patch-size 8         \
+    --backbone patch --timestamp 'patch-model'
+
 
 python main.py                       \
     --num-classes 5 --num-workers 12 \
@@ -13,23 +32,49 @@ python main.py                       \
     --log-dir 'tf-logs' --lr 1e-3    \
     --input-size 480 --kernel-size 9 \
     --dim 384 --patch-size 8         \
-    --backbone patch --timestamp 'patch-model'
+    --backbone patch --timestamp 'patch-model' --valid
 
 python main.py \
     --num-classes 5 --num-workers 12 \
     --num-epochs 80 --batch-size 32  \
-    --valid-data-dir 'data/DDR/test' \
+    --valid-data-dir 'data/DDR/valid' \
     --log-dir 'tf-logs' --lr 1e-3    \
     --input-size 480 --kernel-size 1 --backbone-lr 2e-5 \
     --backbone densenet121 --pretrained --timestamp 'densenet-model-pretrained-unfreezed'
 
+
 python main.py \
     --num-classes 5 --num-workers 12 \
     --num-epochs 80 --batch-size 32  \
     --valid-data-dir 'data/DDR/test' \
     --log-dir 'tf-logs' --lr 1e-3    \
     --input-size 480 --kernel-size 1 --backbone-lr 2e-5 \
+    --backbone densenet121 --pretrained --timestamp 'densenet-model-pretrained-unfreezed' --valid
+
+python main.py \
+    --num-classes 5 --num-workers 12 \
+    --num-epochs 80 --batch-size 32  \
+    --valid-data-dir 'data/DDR/valid' \
+    --log-dir 'tf-logs' --lr 1e-3    \
+    --input-size 480 --kernel-size 1 --backbone-lr 2e-5 \
     --backbone efficientnet --pretrained --timestamp 'efficientnet-model-pretrained-unfreezed'
+
+
+python main.py \
+    --num-classes 5 --num-workers 12 \
+    --num-epochs 80 --batch-size 32  \
+    --valid-data-dir 'data/DDR/test' \
+    --log-dir 'tf-logs' --lr 1e-3    \
+    --input-size 480 --kernel-size 1 --backbone-lr 2e-5 \
+    --backbone efficientnet --pretrained --timestamp 'efficientnet-model-pretrained-unfreezed' --valid
+
+python main.py \
+    --num-classes 5 --num-workers 12 \
+    --num-epochs 80 --batch-size 128  \
+    --valid-data-dir 'data/DDR/valid' \
+    --log-dir 'tf-logs' --lr 1e-3    \
+    --input-size 480 --kernel-size 1 --freeze-backbone \
+    --backbone convnext_base --pretrained --timestamp 'convnext-model-pretrained-freezed'
 
 python main.py \
     --num-classes 5 --num-workers 12 \
@@ -37,7 +82,17 @@ python main.py \
     --valid-data-dir 'data/DDR/test' \
     --log-dir 'tf-logs' --lr 1e-3    \
     --input-size 480 --kernel-size 1 --freeze-backbone \
-    --backbone convnext_base --pretrained --timestamp 'convnext-model-pretrained-freezed'
+    --backbone convnext_base --pretrained --timestamp 'convnext-model-pretrained-freezed' --valid
+
+python main.py \
+    --num-classes 5 --num-workers 12 \
+    --num-epochs 80 --batch-size 32  \
+    --valid-data-dir 'data/DDR/valid' \
+    --log-dir 'tf-logs' --lr 2e-5    \
+    --input-size 480 --kernel-size 1 --only-fc \
+    --backbone convnext_base --pretrained --timestamp 'convnext-fc-pretrained'
+
+
 
 python main.py \
     --num-classes 5 --num-workers 12 \
@@ -45,4 +100,4 @@ python main.py \
     --valid-data-dir 'data/DDR/test' \
     --log-dir 'tf-logs' --lr 2e-5    \
     --input-size 480 --kernel-size 1 --only-fc \
-    --backbone convnext_base --pretrained --timestamp 'convnext-fc-pretrained'
+    --backbone convnext_base --pretrained --timestamp 'convnext-fc-pretrained' --valid
